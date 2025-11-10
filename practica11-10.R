bca9a29124960e7244430909fc23a8f52f1e34b2
Juli Kovalivker <julikovalivker@gmail.com>
2025-11-10 16:51:58 -0300

    Clase 10/11

--- practica11-10.R
+++ practica11-10.R
@@ -18,8 +18,7 @@ b1 # Cuando aumentamos un gramo la dosis, aumenta 4.08 el ritmo
 ## c ##
 
 # 0.05 > 6.25e-07 => Rechazo H0
-# Hay evidencia significativa que la dosis de atenolol
-# disminuye el ritmo cardiaco
+# Hay evidencia significativa que la dosis de atenolol disminuye el ritmo cardiaco
 
 ## d ##
 
@@ -28,9 +27,11 @@ b1 # Cuando aumentamos un gramo la dosis, aumenta 4.08 el ritmo
 ## e ##
 
 # ic1-alfa = b1^ +- talfa/2*se^(b1^), ponemos alfa = 0.05
+
 icobs_izq <- 4.0879 - 2.2010*0.4020
 icobs_der <- 4.0879 + 2.2010*0.4020
 
 ## f ##
+
 
 
